bb07628933016944441fb00cab78d75381239ded
Drew Naylor <DrewNaylor@users.noreply.github.com>
2020-03-25 22:50:38 -0400

    Update AssemblyInfo.

--- drews-libs-winforms/libscrollswitchtabs/My Project/AssemblyInfo.vb
+++ drews-libs-winforms/libscrollswitchtabs/My Project/AssemblyInfo.vb
@@ -8,17 +8,17 @@ Imports System.Runtime.InteropServices
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("libscrollswitchtabs")> 
-<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyTitle("libscrollswitchtabs")>
+<Assembly: AssemblyDescription("Switch tabs in the Windows Forms TabControl using the mouse scroll wheel, like many Linux applications.")>
 <Assembly: AssemblyCompany("")> 
-<Assembly: AssemblyProduct("libscrollswitchtabs")> 
-<Assembly: AssemblyCopyright("Copyright ©  2020")> 
+<Assembly: AssemblyProduct("libscrollswitchtabs")>
+<Assembly: AssemblyCopyright("Copyright (C) 2020 Drew Naylor. Licensed under Apache License 2.0.")>
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("754f3e56-7cc6-42c3-9f5d-70151665f8fe")> 
+<Assembly: Guid("754f3e56-7cc6-42c3-9f5d-70151665f8fe")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("0.1.0.0")>
+<Assembly: AssemblyFileVersion("0.1.0.0")>
